2425c36f0ecf47253e294c08d2c8521f8fa503fe
shmanth <bcshamanth@gmail.com>
2021-10-31 11:22:05 +0530

    added a new ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,18 @@
 
 
 print('adding all ml related code here')
+
+
+
+def model1():
+    print("adding code for ml model 1")
+
+
+
+def model2():
+    print("adding code for ml model 2")
+    
+
+def model3():
+    print("adding code for ml model 3")
+    
